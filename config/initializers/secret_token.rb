49931a4db9d613047ef4bdcd83f389afeac17a87
Alejandro Foullon <alejandro.foullon@gmail.com>
2012-06-24 08:17:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CentralInfoMederi::Application.config.secret_token = 'c1eb2c5d754802fcb17ea37707e709885dcd1accba4e52e9a4f092e0e7f3366da029f79f43dbb6cb293bf683bd75f665e437cf2a1800139051b6606d614b10d0'
